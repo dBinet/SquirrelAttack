712aa86588df4a5c044c4fccb62d3376f7396318
dBinet <d-binet@hotmail.com>
2025-06-06 19:52:12 -0400

    Center card and add generated texture

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,3 +4,5 @@ func _ready():
     var card_scene := preload("res://scenes/Card.tscn")
     var card := card_scene.instantiate()
     add_child(card)
+    card.position = get_viewport_rect().size / 2
+
